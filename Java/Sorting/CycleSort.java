a88e3e523a76640542c9de0350ad05110e20821e
Avantika Chauhan <101965370+avantikachauhann@users.noreply.github.com>
2023-07-11 21:26:18 +0530

    Update CycleSort.java

--- Java/Sorting/CycleSort.java
+++ Java/Sorting/CycleSort.java
@@ -1,27 +1,24 @@
 /*
-Issue#11
-
-Approach:
 Cycle sort is an in-place, unstable sorting algorithm that minimizes the number of writes or swaps needed to sort an array. It works by dividing the array into cycles, where each cycle represents a permutation cycle of the elements. The algorithm moves each element to its correct position within its cycle, ensuring that every element is eventually placed in its final sorted position.
 
-Start with the first element in the array.
-Find the correct position (index) for the current element.
-If the current element is already in its correct position, move to the next element.
-If the current element is not in its correct position, swap it with the element at its correct position.
-Increment the number of writes or swaps.
-Continue steps 2-5 until the current element is in its correct position.
-Repeat steps 2-6 for the remaining elements in the array.
-The cycle sort algorithm repeats this process until all elements are in their correct sorted positions.
+Approach:
+1. Start with the first element in the array.
+2. Find the correct position (index) for the current element.
+3. If the current element is already in its correct position, move to the next element.
+4. If the current element is not in its correct position, swap it with the element at its correct position.
+5. Increment the number of writes or swaps.
+6. Continue steps 2-5 until the current element is in its correct position.
+7. Repeat steps 2-6 for the remaining elements in the array.
+8. The cycle sort algorithm repeats this process until all elements are in their correct sorted positions.
 
-The time complexity of cycle sort = O(n^2).
-The space complexity of cycle sort = O(1).
+Time Complexity: The time complexity of cycle sort = O(n^2).
+Space Complexity: The space complexity of cycle sort = O(1).
 
 Sample Input:
 Array before sorting: 64 34 25 12 22 11 90
 
 Sample Output:
 Array after sorting: 11 12 22 25 34 64 90
-
 */
 
 public class CycleSortExample {
