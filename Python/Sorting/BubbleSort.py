9d209583a612bd95d3a42829897078d2bc55025f
adiel dror <adiel.dror2@gmail.com>
2023-09-06 21:10:13 +0300

    add tree sort + fixed BubbleSort

--- Python/Sorting/BubbleSort.py
+++ Python/Sorting/BubbleSort.py
@@ -8,9 +8,10 @@ Time Complexity: O(n^2) - Quadratic time complexity since we have nested loops i
 Space Complexity: O(1) - Bubble sort is an in-place sorting algorithm, hence no extra space is required.
 '''
 
+
 def bubble_sort(arr):
     n = len(arr)
-    
+
     for i in range(n):
         # The last i elements are already in place
         for j in range(n - i - 1):
@@ -18,16 +19,15 @@ def bubble_sort(arr):
             # than the next element
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    
+
     return arr
-    
+
+
 # Sample input
 arr = [64, 34, 25, 12, 22, 11, 90]
 print("Original array:", arr)
 print("Sorted array:", bubble_sort(arr))
 
-#Sample Output:
-Original array: [64, 34, 25, 12, 22, 11, 90]
-Sorted array: [11, 12, 22, 25, 34, 64, 90]
-
-
+# Sample Output:
+# Original array: [64, 34, 25, 12, 22, 11, 90]
+# Sorted array: [11, 12, 22, 25, 34, 64, 90]
