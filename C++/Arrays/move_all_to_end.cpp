e07c6bc4a9445aaf69018d5f95e4c3a92276e87f
It's-Raj <127078414+raj-khanna1@users.noreply.github.com>
2023-10-23 22:45:27 +0530

    Update move_all_to_end.cpp

--- C++/Arrays/move_all_to_end.cpp
+++ C++/Arrays/move_all_to_end.cpp
@@ -1,15 +1,41 @@
-#include<bits/stdc++.h>
+// ------------>  Move all Zeros to the end of the array <-----------------
+// Algorithm:
+// First, using a loop, we will place the pointer j. If we don’t find any 0, we will not perform the following steps.
+// After that, we will point i to index j+1 and start moving the pointer using a loop.
+// While moving the pointer i, we will do the following:
+// If a[i] != 0 i.e. a[i] is a non-zero element: We will swap a[i] and a[j]. Now, the current j is pointing to the non-zero element a[i]. So, we will shift the pointer j by 1 so that it can again point to the first zero.
+// Finally, our array will be set in the right manner.
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int ar[]={12,30,0,1,0,2,0,0,5,11,0},size=11,index=0;
-    for(int i=0; i<size; i++){
-        if(ar[i]!=0){
-            ar[index++]=ar[i];
+vector<int> moveZeros(int n, vector<int> a) {
+    int j = -1;
+    //place the pointer j:
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            j = i;
+            break;
         }
     }
-    for(int i=index; i<size; i++)
-    ar[i]=0;
-    for(int i=0; i<size; i++)
-    cout<<ar[i]<<" ";
-return 0;
+    //no non-zero elements:
+    if (j == -1) return a;
+    //Move the pointers i and j
+    //and swap accordingly:
+    for (int i = j + 1; i < n; i++) {
+        if (a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
+    return a;
+}
+int main()
+{
+    vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n = 10;
+    vector<int> ans = moveZeros(n, arr);
+    for (auto &it : ans) {
+        cout << it << " ";
+    }
+    cout << '\n';
+    return 0;
 }
